57e850826f7132a5e8896eb9adc275b49f81b314
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-09-07 01:08:02 -0700

    server up

--- chatbot.py
+++ chatbot.py
@@ -6,34 +6,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_PROMPT = "You are a chatbot Answering questions about me, Srihari Krishna. You've been trained on custom data about me, Srihari. Limit your responses to be under 50 words"
+BASE_PROMPT = "You are a chatbot answering questions about me, Srihari Krishna. You've been trained on custom data about me, Srihari. Limit your responses to 0-2 sentences. You should jump directly into the answer, do not preface it with \'Answer:\'"
+filename = "index.json"
 
-def construct_index(directory_path):
-    # set maximum input size
+
+def create_index(directory_path):
     max_input_size = 4096
-    # set number of output tokens
     num_outputs = 2000
-    # set maximum chunk overlap
     max_chunk_overlap = 20
-    # set chunk size limit
     chunk_size_limit = 600
-    # define prompt helper
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-    # define LLM
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
     documents = SimpleDirectoryReader(directory_path).load_data()
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
     index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
-    index.save_to_disk('index.json')
+    index.save_to_disk(filename)
     return index
 
-def ask_ai():
-    index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    while True:
-        Query = input("What do you want to ask? ")
-        response = index.query(f"{BASE_PROMPT}\nUser Query: {Query}")
-        print(f"Response: {response.response}")
+def ask_chatbot(user_input):
+    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+    if not os.path.exists(filename):
+        create_index("context_data/data")
+    index = GPTSimpleVectorIndex.load_from_disk(filename)
+    # user_input = input("What do you want to ask? ")
+    res = index.query(f"{BASE_PROMPT}\nUser Query: {user_input}")
+    return res.response
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-construct_index("context_data/data")
-ask_ai()
+if __name__ == "__main__":
+    print(ask_chatbot("Dummy input"))
---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
+from chatbot import ask_chatbot
+app = Flask(__name__)
+CORS(app) 
+
+@app.route('/chatbot', methods=['GET'])
+def get_hello():
+    try:
+        data = request.get_json()
+        user_query = data.get("ask")
+        response = ask_chatbot(user_query)
+        return jsonify({"response": response})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
